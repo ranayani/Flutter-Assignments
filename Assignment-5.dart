b43aaff92fd8a6e4245fd8c1c18c4de9a57828e6
ranayani <ra_mrwanayani@hotmail.com>
2023-03-11 02:38:16 +0500

    Worked on Assignment 5

--- Assignment-5.dart
+++ Assignment-5.dart
@@ -1,24 +1,59 @@
 import 'dart:math';
 
 void main() {
+  // Q1: Map<String, double> expenses = {
+  //   'sun': 3000.0,
+  //   'mon': 3000.0,
+  //   'tue': 3234.0,
+  // };
+  // Check if "fri" exist in expanses; if exist change it's value to 5000.0 otherwise add 'fri' to expenses and set its value to 5000.0 then print expenses.
+  expensesPrint({
+    'sun': 3000.0,
+    'mon': 3000.0,
+    'tue': 3234.0,
+  });
   // Q2: Write a program in Dart that find the area of a circle using function.
   areaOfCircle(20);
   // Q3: Write a program in a dart that implements the Pythagorean theorem using function.
   pythagoreanTheorem(5, 10);
   // Q4: Write a program in Dart to reverse a String using function.
   reverseString("Hello");
+
+  // Q5: Write a program in Dart to signup user by using username, email & password as required fields while  phoneNumber is optional using function, call this function at least 3 times to create different users. Now create another function named as loginUser and then try to login with correct & incorrect credentials
+  Map<String, dynamic> user1 =
+      signup("Raza Ali", "ra_mrwanayani@hotmail.com", "1234");
+  Map<String, dynamic> user2 =
+      signup("Sabeel", "sabeel@gmail.com", "987654", 123456789);
+  Map<String, dynamic> user3 =
+      signup("Owais", "codewithowais@gmail.com", "654321");
+
+  List users = [user1, user2, user3];
+
+  login("codewithowais@gmail.com", "654321", users); // login successfull
+  login("sabeel@gmail.com", "1234", users); // login failed
+
   // Q6:  Write a program in Dart to calculate power of a certain numbers using function only.
-  calculatePower(10, 0);
+  calculatePower(10, 2);
   // Q7: Write a function to calculate number of vowels and consonant in a String.
   calculateVowelsAndConsonant('number of vowels and consonant in a String');
 }
 
-areaOfCircle(r) {
+expensesPrint(expenses) {
+  Map<String, double> newExpense = {'fri': 5000.0};
+  if (expenses.containsKey("fri")) {
+    expenses["fri"] = 5000.0;
+  } else {
+    expenses.addEntries(newExpense.entries);
+  }
+  print("Expenses $expenses");
+}
+
+areaOfCircle(num r) {
   num pie = 3.14159;
   print("Area of cricle for given radious is ${pie * (r * r)}");
 }
 
-pythagoreanTheorem(a, b) {
+pythagoreanTheorem(num a, num b) {
   num c;
   c = sqrt(a * a + b * b);
   print(
@@ -31,6 +66,34 @@ reverseString(String value) {
     result += value[i];
   }
   print("Reversed string is $result");
+}
+
+Map<String, dynamic> signup(String username, String email, String password,
+    [phoneNumber]) {
+  return {
+    "username": username,
+    "email": email,
+    "password": password,
+    "phoneNumber": phoneNumber
+  };
+}
+
+login(String email, String password, List users) {
+  bool isAuthenticated = false;
+  String username = "";
+  for (var user in users) {
+    if (user["email"] == email && user["password"] == password) {
+      isAuthenticated = true;
+      username = user["username"];
+      break;
+    }
+  }
+
+  if (isAuthenticated) {
+    print("Login Successfully. Welcome $username!");
+  } else {
+    print("Invalid username or password");
+  }
 }
 
 calculatePower(int base, int power) {
